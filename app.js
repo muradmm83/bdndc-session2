9a9ae9d706eddc88e8a919749d47a5391a94c68e
Murad Makhashin <murad.makhashin@daraltamleek.com>
2019-04-01 22:30:59 +0300

    Completed code

--- app.js
+++ app.js
@@ -7,22 +7,41 @@ const be = require('blockexplorer');
 
 const getBlockHash = function (index) {
     // use blockexplorer to get block hash
+
+    return be.blockIndex(index);
 }
 
 const getBlock = function (hash) {
     // use blockexplorer to get block
+
+    return be.block(hash);
 }
 
 const getTransactions = function (...tx) {
     // use blockexplorer to get tranasctions
+
+    let transactionPromises = tx.map(t => be.tx(t));
+
+    return Promise.all(transactionPromises);
 }
 
-const main = async function() {
+const main = async function () {
     // run your code here, consider this as the main method in C#, Java
 
     let blockIndex = 2600; // you could use any index you like
 
     // call your functions here (getBlockHash, getBlock, & getTransactions)
+
+    try {
+        let hash = JSON.parse(await getBlockHash(blockIndex)).blockHash;
+        let block = JSON.parse(await getBlock(hash));
+
+        let tranasctions = await getTransactions(block.tx);
+
+        console.log(tranasctions);
+    } catch (err) {
+        console.log(err)
+    }
 };
 
 main();
